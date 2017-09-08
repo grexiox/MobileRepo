ed50a114b5a2f92396990866bca1dd83610a4d6e
HILL\greg <grzegorz.janoszek@gmail.com>
2017-09-08 09:31:15 +0200

    Build xaml main Screen

---
+++ StatusQueue/StatusQueue/StatusQueue/ViewModels/MainScreenViewModel.cs
@@ -0,0 +1,17 @@
+﻿using StatusQueue.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StatusQueue.ViewModels
+{
+    public class MainScreenViewModel : BaseViewModel
+    {
+        public MainScreenViewModel()
+        {
+            SelectedPostOffices = new List<PostOffice>();
+            SelectedPostOffices.Add(new PostOffice { PostalCode = "pp", OpeningHours = "tt", Street = "str" });
+        }
+        public IList<PostOffice> SelectedPostOffices { get; set; }
+    }
+}
